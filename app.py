0f690c1228a5b4333657fbd937adde593f6fcb50
AdderlyMendoza <aderly19xd@ejemplo.com>
2024-09-23 14:23:13 -0500

    add bootstrap e interfaz

--- app.py
+++ app.py
@@ -3,10 +3,5 @@ import eel
 # Inicializa Eel apuntando al directorio 'web'
 eel.init('web')
 
-# Exponer función de Python para ser llamada desde JavaScript
-@eel.expose
-def saludo_desde_python(nombre):
-    print(f"HOLA, {nombre} desde Python!")  # Mensaje más claro
-
 # Iniciar la aplicación y abrir el archivo 'index.html'
 eel.start('index.html', size=(400, 300))  # Ajustar el tamaño de la ventana
